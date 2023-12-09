a638dad8c329919b0d455710733656880a761d30
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-09 09:31:14 +0100

    improved base code

--- day.py
+++ day.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
 
-def xxx(filename):
+def process(filename):
     for line in open(filename):
         line = line.strip()
 
 if len(sys.argv) > 1:
-    xxx(sys.argv[1])
+    process(sys.argv[1])
 else:
-    xxx('input/day-4.txt')
+    process('input/day-4.txt')
