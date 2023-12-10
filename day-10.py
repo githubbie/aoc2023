b7293df02f142244a9406b505c1697cbd14d29ba
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-10 10:26:17 +0100

    Day 10 (part II)

--- day-10.py
+++ day-10.py
@@ -22,7 +22,7 @@ def replace_start(maze, size, start):
     else:
         print(f'Failed: {size} {start} {north} {east} {south} {west}')
     print(f'Start symbol: {size} {start} {north} {east} {south} {west}: {symbol}')
-    maze[start[1]] = maze[start[1]].replace('S', symbol)
+    maze[start[1]][start[0]] = symbol
 
 # (dx,dy) per symbol
 STEP = { '|': [( 0,-1), ( 0,+1)],
@@ -45,13 +45,40 @@ def take_step(maze, now, previous):
         return option2, now
 
 def furthest_point(maze, start):
+    clean_maze = [['.']*len(maze[0]) for i in range(len(maze))]
     now, previous = take_step(maze, start, (-2,-2))
+    clean_maze[now[1]][now[0]] = maze[now[1]][now[0]]
+    #print(f'clean_maze {now[1]}: {"".join(clean_maze[now[1]])}')
     steps = 1
     while now != start:
         print(f'After {steps} steps, now in {now} (coming from {previous})', end=' ')
         now, previous = take_step(maze, now, previous)
+        clean_maze[now[1]][now[0]] = maze[now[1]][now[0]]
+        #print(f'clean_maze {now[1]}: {"".join(clean_maze[now[1]])}')
         steps += 1
-    return int(steps/2)
+    return int(steps/2), clean_maze
+
+def mark_inout(maze):
+    count_in = 0
+    for y in range(len(maze)):
+        out = True
+        last_non_dash = ''
+        for x in range(len(maze[y])):
+            if maze[y][x] == '.':
+                if out:
+                    maze[y][x] = ' '
+                else:
+                    maze[y][x] = '*'
+                    count_in+=1 
+            elif maze[y][x] == 'J' and last_non_dash == 'F':
+                out = not out
+            elif maze[y][x] == '7' and last_non_dash == 'L':
+                out = not out
+            elif maze[y][x] == '|':
+                out = not out
+            if maze[y][x] != '-': 
+                last_non_dash = maze[y][x]
+    return count_in
 
 def process(filename):
     maze = []
@@ -59,14 +86,21 @@ def process(filename):
     start = (-1,-1)
     for line in open(filename):
         line = line.strip()
-        maze.append(line)
+        maze.append(list(line))
         start_x = line.find('S')
         if start_x > -1:
             start = (start_x, line_no)
         line_no+=1
     maze_size = (len(maze[0]),len(maze))
     replace_start(maze, maze_size, start)
-    print(furthest_point(maze, start))
+    distance, clean_maze = furthest_point(maze, start)
+    print(distance)
+    for line in clean_maze:
+        print(''.join(line))
+    count_in = mark_inout(clean_maze)
+    for line in clean_maze:
+        print(''.join(line))
+    print(count_in)
 
 if len(sys.argv) > 1:
     process(sys.argv[1])
