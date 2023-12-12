c0073503739b20ee1a301ab397cd7942e80b952e
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-12 21:05:34 +0100

    day 12 (part II)

--- day-12.py
+++ day-12.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 import sys
 
-def count(line, position, damaged):
-    print(f'count({line}, {position}, {damaged} = ')
+def count(line, position, damaged, cache):
     # count the number of solutions for finding the sequence of 
     # of damaged springs in damaged, in the string line, starting
     # at position
     # Assumptions:
     # - the first damaged spring can start immediately at position
+    if position in cache:
+        if damaged in cache[position]:
+            return cache[position][damaged]
+
+    def update_cache(result):
+        if not position in cache:
+            cache[position] = {}
+        cache[position][damaged] = result
+
     at_least_remaining = sum(damaged)+len(damaged)-1
-    print('  ', len(line)-position, at_least_remaining)
     if len(line)-position < at_least_remaining:
-        print('(a) 0')
+        update_cache(0)
         return 0
-    if damaged == []:
+    if damaged == ():
         if line.find('#', position) == -1:
-            print('(b) 1')
+            update_cache(1)
             return 1
         else:
-            print('(c) 0')
+            update_cache(0)
             return 0
     next_qm = line.find('?', position)
     next_hs = line.find('#', position)
@@ -30,27 +37,26 @@ def count(line, position, damaged):
         next_possible = min(next_qm, next_hs)
     if next_possible > -1:
         needed = damaged[0]
-        print('    ', next_possible, needed, len(line))
         if (next_possible + needed == len(line)) or \
            ((next_possible + needed < len(line)) and (line[next_possible+needed] in '?.')):
             allowed = not '.' in line[next_possible:next_possible+needed]
         else:
             allowed = False
         if allowed and line[next_possible] == '?':
-            result = count(line, next_possible+needed+1, damaged[1:]) + \
-                     count(line, next_possible+1, damaged)
-            print(f'(d) {result}')
+            result = count(line, next_possible+needed+1, damaged[1:], cache) + \
+                     count(line, next_possible+1, damaged, cache)
+            update_cache(result)
             return result
         elif allowed and line[next_possible] == '#':
-            result = count(line, next_possible+needed+1, damaged[1:])
-            print(f'(e) {result}')
+            result = count(line, next_possible+needed+1, damaged[1:], cache)
+            update_cache(result)
             return result
         elif not allowed and line[next_possible] == '?':
-            result = count(line, next_possible+1, damaged)
-            print(f'(f) {result}')
+            result = count(line, next_possible+1, damaged, cache)
+            update_cache(result)
             return result
 
-    print('(g) 0')
+    update_cache(0)
     return 0
 
 def process(filename):
@@ -59,7 +65,10 @@ def process(filename):
         line = line.strip()
         s, *damaged = line.replace(',',' ').split()
         damaged = [int(i) for i in damaged]
-        total += count(s, 0, damaged)
+        s = '?'.join([s]*5)
+        damaged = damaged*5
+        total += count(s, 0, tuple(damaged), {})
+        print(f'{line}: {total}')
     print(total)
 
 if len(sys.argv) > 1:
