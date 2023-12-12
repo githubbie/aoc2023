2d302ff8bdc8d3e6a939aabf8997ecbb9227155d
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-12 21:07:36 +0100

    using functools.cache

--- day-12.py
+++ day-12.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
 import sys
+from functools import cache
 
-def count(line, position, damaged, cache):
+@cache
+def count(line, position, damaged):
     # count the number of solutions for finding the sequence of 
     # of damaged springs in damaged, in the string line, starting
     # at position
     # Assumptions:
     # - the first damaged spring can start immediately at position
-    if position in cache:
-        if damaged in cache[position]:
-            return cache[position][damaged]
-
-    def update_cache(result):
-        if not position in cache:
-            cache[position] = {}
-        cache[position][damaged] = result
 
     at_least_remaining = sum(damaged)+len(damaged)-1
     if len(line)-position < at_least_remaining:
-        update_cache(0)
         return 0
     if damaged == ():
         if line.find('#', position) == -1:
-            update_cache(1)
             return 1
         else:
-            update_cache(0)
             return 0
     next_qm = line.find('?', position)
     next_hs = line.find('#', position)
@@ -43,20 +34,15 @@ def count(line, position, damaged, cache):
         else:
             allowed = False
         if allowed and line[next_possible] == '?':
-            result = count(line, next_possible+needed+1, damaged[1:], cache) + \
-                     count(line, next_possible+1, damaged, cache)
-            update_cache(result)
+            result = count(line, next_possible+needed+1, damaged[1:]) + \
+                     count(line, next_possible+1, damaged)
             return result
         elif allowed and line[next_possible] == '#':
-            result = count(line, next_possible+needed+1, damaged[1:], cache)
-            update_cache(result)
+            result = count(line, next_possible+needed+1, damaged[1:])
             return result
         elif not allowed and line[next_possible] == '?':
-            result = count(line, next_possible+1, damaged, cache)
-            update_cache(result)
+            result = count(line, next_possible+1, damaged)
             return result
-
-    update_cache(0)
     return 0
 
 def process(filename):
@@ -67,7 +53,7 @@ def process(filename):
         damaged = [int(i) for i in damaged]
         s = '?'.join([s]*5)
         damaged = damaged*5
-        total += count(s, 0, tuple(damaged), {})
+        total += count(s, 0, tuple(damaged))
         print(f'{line}: {total}')
     print(total)
 
