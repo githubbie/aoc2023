9cd22241005fb7c3f10fa91e54bd47ab865d2731
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-14 07:47:32 +0100

    day 13 (part II)

--- day-13.py
+++ day-13.py
@@ -2,20 +2,21 @@
 import sys
 
 def find_reflections(vector):
-    print(f'find_reflections({vector}):')
+    print(f'find_reflections({vector}):', end='')
     result = []
     for i in range(1, len(vector)):
         if i <= len(vector)/2:
-            print(vector[:i], list(reversed(vector[i:2*i])))
+            #print(vector[:i], list(reversed(vector[i:2*i])))
             comparison = zip(vector[:i],reversed(vector[i:2*i]))
         else:
-            print(vector[2*i-len(vector):i], list(reversed(vector[i:])))
+            #print(vector[2*i-len(vector):i], list(reversed(vector[i:])))
             comparison = zip(vector[2*i-len(vector):i],reversed(vector[i:]))
         equal = [a==b for a,b in comparison]
-        print(i, list(comparison), equal, all(equal))
+        #print(i, list(comparison), equal, all(equal))
         if all(equal):
             result.append(i)
-    return result
+    print(result)
+    return set(result)
 
 def handle_map(horizontal, vertical):
     print(horizontal)
@@ -24,9 +25,28 @@ def handle_map(horizontal, vertical):
     print(find_reflections(vertical))
     return sum(find_reflections(horizontal))+100*sum(find_reflections(vertical))
 
+def handle_map_part2(horizontal, vertical):
+    original_horizontal = find_reflections(horizontal)
+    original_vertical = find_reflections(vertical)
+    total = 0
+    for h in range(len(vertical)):
+        for v in range(len(horizontal)):
+            print(h, v, horizontal[v], vertical[h])
+            horizontal[v] = horizontal[v] ^ (1 << (len(vertical)-h-1))
+            vertical[h] = vertical[h] ^ (1 << (len(horizontal)-v-1))
+            print(h, v, horizontal[v], vertical[h])
+            new_horizontal = find_reflections(horizontal) - original_horizontal
+            new_vertical = find_reflections(vertical) - original_vertical
+            if new_horizontal or new_vertical:
+                print(f'result: {sum(new_horizontal)+100*sum(new_vertical)}')
+                return sum(new_horizontal)+100*sum(new_vertical)
+            horizontal[v] = horizontal[v] ^ (1 << (len(vertical)-h-1))
+            vertical[h] = vertical[h] ^ (1 << (len(horizontal)-v-1))
+            print(h, v, horizontal[v], vertical[h])
+
 def process(filename):
     horizontal, vertical = [], []
-    total = 0
+    total1, total2 = 0, 0
     for line in open(filename):
         line = line.strip()
         if line != '':
@@ -37,12 +57,15 @@ def process(filename):
             horizontal = [s+line[i] for i,s in enumerate(horizontal)]
         else:
             horizontal = [int(s,2) for s in horizontal]
-            total += handle_map(horizontal, vertical)
+            total1 += handle_map(horizontal, vertical)
+            total2 += handle_map_part2(horizontal, vertical)
             horizontal, vertical = [], []
     # last map
     horizontal = [int(s,2) for s in horizontal]
-    total += handle_map(horizontal, vertical)
-    print(total)
+    total1 += handle_map(horizontal, vertical)
+    total2 += handle_map_part2(horizontal, vertical)
+    print(total1)
+    print(total2)
 
 if len(sys.argv) > 1:
     process(sys.argv[1])
