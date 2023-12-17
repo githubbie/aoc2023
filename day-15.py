77d55001279f3d0b8533e3ef45301a320b534f7b
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-17 19:50:15 +0100

    Day 15 (part II)

--- day-15.py
+++ day-15.py
@@ -7,11 +7,46 @@ def hash(s):
         current = ((current+ord(c)) * 17) % 256
     return current
 
+def do(instruction, boxes):
+    label, length = instruction[:-1], instruction[-1]
+    if length == '-':
+        # remove
+        box = hash(label)
+        if label in boxes[box]:
+            del boxes[box][label]
+    else:
+        label = label[:-1]
+        box = hash(label)
+        boxes[box][label] = int(length)
+
+def print_boxes(boxes):
+    for i, box in enumerate(boxes):
+        if box:
+            print(f'Box {i}:', end='')
+            for label in box:
+                print(f' [{label} {box[label]}]', end='')
+            print()
+
+def power(boxes):
+    def p(box):
+        return sum([(i+1)*length for i, length in enumerate(box.values())])
+    return sum([(i+1)*p(box) for i,box in enumerate(boxes)])
+
 def process(filename):
     for line in open(filename):
         line = line.strip()
         total = sum([hash(instruction) for instruction in line.split(',')])
         print(total)
+        boxes = []
+        for i in range(256):
+            boxes.append({})
+        print('Initialize')
+        print_boxes(boxes)
+        for instruction in line.split(','):
+            print('Instruction:', instruction)
+            do(instruction, boxes)
+            print_boxes(boxes)
+        print(power(boxes))
 
 if len(sys.argv) > 1:
     process(sys.argv[1])
