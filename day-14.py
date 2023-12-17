9378359071cb210fb3f6ebed2d4e486757d8869a
Remco van Engelen <remco.van.engelen@gmail.com>
2023-12-17 19:17:23 +0100

    Day 14 (part II)

--- day-14.py
+++ day-14.py
@@ -8,11 +8,11 @@ def print_map(map):
     for s in map:
         print(s)
 
-def shift_left_row(s):
-    return '#'.join([''.join(sorted(p, reverse=True)) for p in s.split('#')])
+def shift_row(s, reverse):
+    return '#'.join([''.join(sorted(p, reverse=reverse)) for p in s.split('#')])
 
-def shift_left_map(map):
-    return [shift_left_row(r) for r in map]
+def shift_map(map, reverse):
+    return [shift_row(r, reverse) for r in map]
 
 def count_row(s):
     return len(s.split('O'))-1
@@ -20,20 +20,47 @@ def count_row(s):
 def count_map(map):
     return sum([(len(map)-i)*count_row(r) for i,r in enumerate(map)])
 
+def tilt(map, direction):
+    map = map.copy()
+    for c in direction.replace('C','NWSE'):
+        if c == 'N':
+            map = transpose(shift_map(transpose(map), True))
+        elif c == 'S':
+            map = transpose(shift_map(transpose(map), False))
+        elif c == 'E':
+            map = shift_map(map, False)
+        elif c == 'W':
+            map = shift_map(map, True)
+    return map
+
 def process(filename):
     for map_s in open(filename).read().split('\n\n'):
         map = map_s.split('\n')
         if map[-1] == '':
             del map[-1]
         print_map(map)
-        print()
-        print_map(transpose(map))
-        print()
-        print_map(shift_left_map(transpose(map)))
-        print()
-        print_map(transpose(shift_left_map(transpose(map))))
-        print()
-        print(count_map(transpose(shift_left_map(transpose(map)))))
+        print(count_map(tilt(map, 'N')))
+        single_steps = tilt(map, 'C')
+        double_steps = tilt(map, 'CC')
+        n = 1
+        while single_steps != double_steps:
+            single_steps = tilt(single_steps, 'C')
+            double_steps = tilt(double_steps, 'CC')
+            n += 1
+        cycle_map = tilt(single_steps, 'C')
+        k = 1
+        while cycle_map != single_steps:
+            cycle_map = tilt(cycle_map, 'C')
+            k += 1
+        number_of_cycles = int((1_000_000_000 - n)/k)
+        remainder = 1_000_000_000-n-k*number_of_cycles
+        print(n, k, number_of_cycles, remainder)
+        final_map = single_steps
+        for _ in range(remainder):
+            final_map = tilt(final_map, 'C')
+        print_map(final_map)
+        print(count_map(final_map))
+
 
 if len(sys.argv) > 1:
     process(sys.argv[1])
